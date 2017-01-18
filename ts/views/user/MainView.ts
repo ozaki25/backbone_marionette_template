8430b163163a0d0421e5e442489619799c2d5ca9
ozaki25 <fs65ugumori@yahoo.co.jp>
2017-01-18 10:58:21 +0900

    ui bug fix

--- ts/views/user/MainView.ts
+++ ts/views/user/MainView.ts
@@ -6,11 +6,6 @@ import MessageView from './MessageView';
 
 export default class MainView extends Marionette.LayoutView<Backbone.Model> {
     template = '#user-main-view';
-    ui = {
-        inputName: '.name',
-        helloBtn : '#hello',
-    };
-
     constructor(options = {}) {
         super(
             _.defaults(options, {
@@ -25,19 +20,37 @@ export default class MainView extends Marionette.LayoutView<Backbone.Model> {
         };
     }
 
+    ui() {
+        return {
+            inputName: '.name',
+            helloBtn : '#hello',
+        };
+    }
+
     events() {
         return {
-            // eventsでuiが使えない
-            // 'click @ui.helloBtn': 'onClickHello'
-            'click #hello': 'onClickHello'
+            'click @ui.helloBtn': 'onClickHello'
         };
     }
 
     onClickHello(e: any) {
         e.preventDefault();
-        const name = (<any>this.ui.inputName).val().trim();
+
+        // ui()で定義しているためthis.ui.xxxとするとtypescriptのコンパイルエラーになる
+        // 利用者はui()で定義するがMarionetteが内部でthis.uiに詰め替えているためこの問題が発生する
+
+        // 対応案1
+        // this.uiを別の変数に詰め直す
+        // その際使用するプロパティをデフォルト値をセットしておかないとエラーになる
+        // let uiElement = _.defaults({}, this.ui, { inputName: <any>'' });
+        // const name = uiElement.inputName.val().trim();
+
+        // 対応案2
+        // this.ui.[xxx]の形式で呼び出す
+        // この形式だとコンパイルをすり抜けることができる
+        const name = this.ui['inputName'].val().trim();
         if(name) {
-            (<any>this.ui.inputName).val('');
+            this.ui['inputName'].val('');
             this.renderMessage(name);
         }
     }
