268c1802e6c98e2418c5c3bc63c7c10f74da3225
ozaki <fs65ugumori@yahoo.co.jp>
2017-07-25 19:52:24 +0900

    module update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
+    'env': {
+        'browser': true,
+        'jquery': true,
+        'node': true,
         "es6": true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'sourceType': 'module'
     },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    'rules': {
+        'indent': ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+        'no-console': ['off'],
+        'space-before-blocks': ['error', { 'functions': 'always', 'keywords': 'always' }],
+        'eqeqeq': ['error', 'always'],
+    },
+    'globals': {
+
+    },
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,49 @@
 'use strict';
 
-let gulp = require('gulp');
-let runSequence = require('run-sequence');
-let browserify = require('browserify');
-let source = require('vinyl-source-stream');
-let webserver = require('gulp-webserver');
-let babelify = require("babelify");
-let eslint = require('gulp-eslint');
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const webserver = require('gulp-webserver');
+const eslint = require('gulp-eslint');
+const streamify = require('gulp-streamify');
+const uglify = require('gulp-uglify');
+const babelify = require('babelify');
 
 gulp.task('build', () => {
-    runSequence(['browserify']);
+    runSequence(['browserify', 'html', 'css', 'fonts']);
 });
 
 gulp.task('browserify', () => {
     browserify('./js/app.js', {
         debug:   true,
-        require: ['moment', 'pikaday', 'zeroclipboard', 'numbro']
+        require: ['moment'],
+        transform: ['jstify'],
     })
-        .transform(babelify)
+        .transform(babelify, { presets: ['es2015'] })
         .bundle()
         .pipe(source('bundle.js'))
+        .pipe(streamify(uglify()))
+        .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('html', () => {
+    gulp.src('html/index.html')
         .pipe(gulp.dest('./dist/'));
 });
 
+gulp.task('css', () => {
+    gulp.src('./css/**')
+        .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('fonts', () => {
+    gulp.src('./fonts/**')
+        .pipe(gulp.dest('./dist/fonts/'));
+});
+
 gulp.task('watch', () => {
-    gulp.watch('./js/**/*.js', ['build', 'lint']);
+    gulp.watch(['./js/**/*.js', './js/**/*.jst', './css/**/*.css', './html/index.html'], ['build', 'lint']);
 });
 
 gulp.task('server', () => {
@@ -38,10 +57,11 @@ gulp.task('server', () => {
 
 gulp.task('lint', () => {
     gulp.src('./js/**/*.js')
-        .pipe(eslint())
+        .pipe(eslint({
+            fix: true
+        }))
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
-
 
 gulp.task('default', ['build', 'watch', 'server', 'lint']);
